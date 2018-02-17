95dda8469edc31d2f31d5215ebc78658c1bcaf87
Stefan Möding <stm@kill-9.net>
2018-02-17 09:01:16 +0100

    Try to fix rspec with hiera

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,18 +10,18 @@ RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
   c.manifest = File.join(fixture_path, '../../manifests/site.pp')
-  c.hiera_config = File.join(fixture_path, '../fixtures/hiera.yaml')
+  c.hiera_config = 'hiera.yaml'
 
   # :id and :osfamily facts are needed for concat module
-  c.default_facts = {
-    :hostname        => 'foo',
-    :domain          => 'example.com',
-    :fqdn            => 'foo.example.com',
-    :id              => 'stm',
-    :osfamily        => 'Debian',
-    :operatingsystem => 'Debian',
-    :concat_basedir  => '/var/tmp',
-  }
+  # c.default_facts = {
+  #   :hostname        => 'foo',
+  #   :domain          => 'example.com',
+  #   :fqdn            => 'foo.example.com',
+  #   :id              => 'stm',
+  #   :osfamily        => 'Debian',
+  #   :operatingsystem => 'Debian',
+  #   :concat_basedir  => '/var/tmp',
+  # }
 
   c.after(:suite) do
     RSpec::Puppet::Coverage.report!
