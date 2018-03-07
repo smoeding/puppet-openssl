5a80a259c21e01a9649711255f16dc75d7a4cdfa
Stefan Möding <stm@kill-9.net>
2018-03-07 19:04:37 +0100

    Move default parameters in tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
 describe 'openssl' do
-  let(:hiera_config) { 'hiera.yaml' }
+  let(:params) do
+    {
+      :default_key_dir       => '/key',
+      :default_cert_dir      => '/crt',
+      :cert_source_directory => '/foo/bar',
+      :root_group            => 'wheel'
+    }
+  end
 
   on_supported_os.each do |os, facts|
     let(:facts) { facts }
 
     context "on #{os} with default parameters" do
-      let(:params) do
-        { :default_key_dir       => '/key',
-          :default_cert_dir      => '/crt',
-          :cert_source_directory => '/foo/bar'
-        }
-      end
-
       it {
         is_expected.to contain_class('openssl')
       }
