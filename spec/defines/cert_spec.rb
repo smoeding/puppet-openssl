2dacb5b9a7964f3dab667f71cdbf108c5ca0bbcd
Stefan Möding <stm@kill-9.net>
2018-03-08 18:51:37 +0100

    Add tests for openssl::cert

---
+++ spec/defines/cert_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe 'openssl::cert' do
+  let(:pre_condition) do
+    'class { "::openssl":
+       default_key_dir       => "/key",
+       default_cert_dir      => "/crt",
+       cert_source_directory => "/foo",
+       root_group            => "wheel"
+     }'
+  end
+
+  before do
+    MockFunction.new('file') do |f|
+      f.stubbed.with('/foo/cert.crt').returns("# /foo/cert.crt\n")
+    end
+  end
+
+  let(:title) { 'cert' }
+
+  on_supported_os.each do |os, facts|
+    let(:facts) { facts }
+
+    context "on #{os} with default parameters" do
+      it {
+        is_expected.to contain_class('openssl')
+
+        is_expected.to contain_concat('/crt/cert.crt').
+          with_owner('root').
+          with_group('wheel').
+          with_mode('0444').
+          with_backup('false').
+          that_requires('Package[openssl]')
+
+        is_expected.to contain_concat__fragment('/crt/cert.crt-cert').
+          with_target('/crt/cert.crt').
+          with_content("# /foo/cert.crt\n").
+          with_order('10')
+      }
+    end
+  end
+end
