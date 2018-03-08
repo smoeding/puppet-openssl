cf047391ecaa9baa620316029ab10260d7d79734
Stefan Möding <stm@kill-9.net>
2018-03-08 20:25:42 +0100

    Update tests for openssl::key

--- spec/defines/key_spec.rb
+++ spec/defines/key_spec.rb
@@ -15,6 +15,7 @@ describe 'openssl::key' do
   before do
     MockFunction.new('file') do |f|
       f.stubbed.with('/foo/key.key').returns("# /foo/key.key\n")
+      f.stubbed.with('/foo/secret.key').returns("# /foo/secret.key\n")
     end
   end
 
@@ -29,6 +30,126 @@ describe 'openssl::key' do
           with_ensure('file').
           with_owner('root').
           with_group('wheel').
+          with_mode('0400').
+          with_content("# /foo/key.key\n").
+          with_backup('false').
+          with_show_diff('false').
+          that_requires('Package[openssl]')
+      }
+    end
+
+    context "on #{os} with key => secret" do
+      let(:params) do
+        { key: 'secret' }
+      end
+
+      it {
+        is_expected.to contain_class('openssl')
+
+        is_expected.to contain_file('/key/secret.key').
+          with_ensure('file').
+          with_owner('root').
+          with_group('wheel').
+          with_mode('0400').
+          with_content("# /foo/key.key\n").
+          with_backup('false').
+          with_show_diff('false').
+          that_requires('Package[openssl]')
+      }
+    end
+
+    context "on #{os} with source => secret" do
+      let(:params) do
+        { source: 'secret' }
+      end
+
+      it {
+        is_expected.to contain_class('openssl')
+
+        is_expected.to contain_file('/key/key.key').
+          with_ensure('file').
+          with_owner('root').
+          with_group('wheel').
+          with_mode('0400').
+          with_content("# /foo/secret.key\n").
+          with_backup('false').
+          with_show_diff('false').
+          that_requires('Package[openssl]')
+      }
+    end
+
+    context "on #{os} with extension => pem" do
+      let(:params) do
+        { extension: 'pem' }
+      end
+
+      it {
+        is_expected.to contain_class('openssl')
+
+        is_expected.to contain_file('/key/key.pem').
+          with_ensure('file').
+          with_owner('root').
+          with_group('wheel').
+          with_mode('0400').
+          with_content("# /foo/key.key\n").
+          with_backup('false').
+          with_show_diff('false').
+          that_requires('Package[openssl]')
+      }
+    end
+
+    context "on #{os} with key_mode => 0642" do
+      let(:params) do
+        { key_mode: '0642' }
+      end
+
+      it {
+        is_expected.to contain_class('openssl')
+
+        is_expected.to contain_file('/key/key.key').
+          with_ensure('file').
+          with_owner('root').
+          with_group('wheel').
+          with_mode('0642').
+          with_content("# /foo/key.key\n").
+          with_backup('false').
+          with_show_diff('false').
+          that_requires('Package[openssl]')
+      }
+    end
+
+    context "on #{os} with key_owner => mysql" do
+      let(:params) do
+        { key_owner: 'mysql' }
+      end
+
+      it {
+        is_expected.to contain_class('openssl')
+
+        is_expected.to contain_file('/key/key.key').
+          with_ensure('file').
+          with_owner('mysql').
+          with_group('wheel').
+          with_mode('0400').
+          with_content("# /foo/key.key\n").
+          with_backup('false').
+          with_show_diff('false').
+          that_requires('Package[openssl]')
+      }
+    end
+
+    context "on #{os} with key_group => mysql" do
+      let(:params) do
+        { key_group: 'mysql' }
+      end
+
+      it {
+        is_expected.to contain_class('openssl')
+
+        is_expected.to contain_file('/key/key.key').
+          with_ensure('file').
+          with_owner('root').
+          with_group('mysql').
           with_mode('0400').
           with_content("# /foo/key.key\n").
           with_backup('false').
