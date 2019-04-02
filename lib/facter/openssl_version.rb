f84cbd44c20668a0a839960accb6b731a50e3769
Stefan Möding <stm@kill-9.net>
2019-04-02 19:02:34 +0200

    Fix Rubocop issues

--- lib/facter/openssl_version.rb
+++ lib/facter/openssl_version.rb
@@ -1,8 +1,8 @@
 # openssl_version.rb --- get OpenSSL version
-if defined?(Facter::Util::Resolution.which) and Facter::Util::Resolution.which('openssl')
+if defined?(Facter::Util::Resolution.which) && Facter::Util::Resolution.which('openssl')
   Facter.add(:openssl_version) do
     setcode do
-      Facter::Util::Resolution.exec('openssl version').lines.first.match(/OpenSSL ([0-9.]+[a-z])/)[1]
+      Facter::Util::Resolution.exec('openssl version').lines.first.match(%r{OpenSSL ([0-9.]+[a-z])})[1]
     end
   end
 end
