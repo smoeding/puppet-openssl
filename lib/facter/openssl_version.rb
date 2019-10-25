f5839532fbf33b5949d91d3a90ec799652ee9757
Stefan Möding <stm@kill-9.net>
2019-10-25 18:49:49 +0200

    Handle openssl version without trailing letter (e.g 1.1.1)

--- lib/facter/openssl_version.rb
+++ lib/facter/openssl_version.rb
@@ -2,7 +2,7 @@
 if defined?(Facter::Util::Resolution.which) && Facter::Util::Resolution.which('openssl')
   Facter.add(:openssl_version) do
     setcode do
-      Facter::Util::Resolution.exec('openssl version').lines.first.match(%r{OpenSSL ([0-9.]+[a-z])})[1]
+      Facter::Util::Resolution.exec('openssl version').lines.first.match(%r{OpenSSL ([0-9.]+[a-z]?)})[1]
     end
   end
 end
